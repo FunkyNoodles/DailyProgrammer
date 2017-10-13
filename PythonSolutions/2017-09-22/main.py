07d99a7ccf4e07affb33e0f9ef98c780a5de4081
Funky Noodles <nickasds@hotmail.com>
2017-10-12 21:15:40 -0700

    Some further optimization

--- PythonSolutions/2017-09-22/main.py
+++ PythonSolutions/2017-09-22/main.py
@@ -6,28 +6,36 @@ import time
 class Index:
     index = 0
     requirement = 0
-    def __init__(self, _index, _requirement):
+    combinations = {}
+    def __init__(self, _index, _requirement, _combinations):
         self.index = _index
         self.requirement = _requirement
+        self.combinations = _combinations
 
     def __lt__(self, other):
-        return self.requirement < other.requirement
+        return len(self.combinations[self.requirement]) < len(other.combinations[other.requirement])
+
 
 def parse_input(_file_name):
     n_flag = False
     _arr = []
     _n = 0
+    _init_grid = None
     with open(_file_name, 'r') as f:
-        for line in f:
-            for s in line.split(' '):
+        for _index, line in enumerate(f):
+            for _j, s in enumerate(line.split(' ')):
                 num = int(s)
-                if not n_flag:
-                    _n = num
-                    n_flag = True
-                    break
-                _arr.append(num)
+                if _index < 2:
+                    if not n_flag:
+                        _n = num
+                        _init_grid = np.zeros(shape=[_n, _n], dtype=int)
+                        n_flag = True
+                        break
+                    _arr.append(num)
+                else:
+                    _init_grid[_index - 2, _j] = num
 
-    return _n, _arr
+    return _n, _arr, _init_grid
 
 def seen_buildings(permutation):
     max_height = -1
@@ -59,6 +67,12 @@ def try_place(_index, _candidate, _n, _grid):
         for i in range(0, _n):
             _c = _candidate[i]
             if _c != _grid[i, sub_index] and _grid[i, sub_index] != 0:
+                # Cannot place because overlap
+                return None
+            perpendicular = _grid[i, :]
+            perpendicular = np.delete(perpendicular, sub_index)
+            if _c in perpendicular:
+                # Cannot place because already exists
                 return None
             new_grid[i, sub_index] = _c
     elif region == 1:
@@ -67,12 +81,22 @@ def try_place(_index, _candidate, _n, _grid):
             _c = _candidate[_n - j - 1]
             if _c != _grid[sub_index, j] and _grid[sub_index, j] != 0:
                 return None
+            perpendicular = _grid[:, j]
+            perpendicular = np.delete(perpendicular, sub_index)
+            if _c in perpendicular:
+                # Cannot place because already exists
+                return None
             new_grid[sub_index, j] = _c
     elif region == 2:
         # Vertical up
         for i in range(0, _n):
             _c = _candidate[i]
             if _c != _grid[_n - 1 - i, _n - 1 - sub_index] and _grid[_n - 1 - i, _n - 1 - sub_index] != 0:
+                return None
+            perpendicular = _grid[_n - 1 - i, :]
+            perpendicular = np.delete(perpendicular, _n - 1 - sub_index)
+            if _c in perpendicular:
+                # Cannot place because already exists
                 return None
             new_grid[_n - 1 - i, _n - 1 - sub_index] = _c
     elif region == 3:
@@ -81,39 +105,49 @@ def try_place(_index, _candidate, _n, _grid):
             _c = _candidate[j]
             if _c != _grid[_n - 1 - sub_index, j] and _grid[_n - 1 - sub_index, j] != 0:
                 return None
+            perpendicular = _grid[:, j]
+            perpendicular = np.delete(perpendicular, _n - 1 - sub_index)
+            if _c in perpendicular:
+                # Cannot place because already exists
+                return None
             new_grid[_n - 1 - sub_index, j] = _c
 
     return new_grid
 
-def solve(_index, _grid, _n, _options, _arr, _combinations):
+def solve(_index, _grid, _n, _options, _arr, _combinations, _nodes_expanded):
     for _candidate in _options:
+        _nodes_expanded += 1
         new_grid = try_place(_arr[_index].index, _candidate, _n, _grid)
-        # print new_grid, _index
         if new_grid is None:
             continue
         if _index == _n * 4 - 1:
             # Found solution
-            return new_grid
+            return new_grid, _nodes_expanded
         next_options = _combinations[_arr[_index + 1].requirement]
-        print new_grid
-        _solved_grid = solve(_index + 1, new_grid, _n, next_options, _arr, _combinations)
+        print new_grid, _index
+        _solved_grid, _nodes_expanded = solve(_index + 1, new_grid, _n, next_options, _arr, _combinations, _nodes_expanded)
         if _solved_grid is not None:
-            return _solved_grid
+            return _solved_grid, _nodes_expanded
 
-    return None
+    return None, _nodes_expanded
 
 
-file_name = 'input4'
+file_name = 'input3'
 
-n, arr = parse_input(file_name)
-index_arr = [Index(idx, req) for idx, req in enumerate(arr)]
-index_arr = sorted(index_arr, reverse=True)
+n, arr, init_grid = parse_input(file_name)
 
 start = time.time()
 combinations = build_combinations(n)
-# print combinations
 
-solved_grid = solve(0, np.zeros(shape=[n, n], dtype=int), n, combinations[index_arr[0].requirement], index_arr, combinations)
+index_arr = [Index(idx, req, combinations) for idx, req in enumerate(arr)]
+index_arr = sorted(index_arr)
+# for i in index_arr:
+#     print i.index, i.requirement
+#
+# for k, v in combinations.iteritems():
+#     print k, len(v)
+
+solved_grid, nodes_expanded = solve(0, init_grid, n, combinations[index_arr[0].requirement], index_arr, combinations, 0)
 print solved_grid
 end = time.time()
-print 'Took:', (end - start), 's'
+print 'Took:', (end - start), 's', '\tNodes Expanded:', nodes_expanded
